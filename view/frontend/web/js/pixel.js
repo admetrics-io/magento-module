e8894b77f479fe5c51dfd823e2f2c2e0600015db
Nils Rückmann <nr@admetrics.io>
2024-09-02 16:08:34 +0200

    move tracking script to head and add scripts after dom ready

--- view/frontend/web/js/pixel.js
+++ view/frontend/web/js/pixel.js
@@ -1,35 +1,37 @@
 define([
+    'jquery',
     'Magento_Customer/js/customer-data',
-], function (customerData) {
+], function ($, customerData) {
     'use strict';
 
     return function (config) {
+        $(function() {
+            const cart = customerData.get("cart");
+            const customer = customerData.get("customer");
 
-        const cart = customerData.get("cart");
-        const customer = customerData.get("customer");
+            cart.subscribe((newCart) => {
+                // @todo: we need anther tracking api to cover async changes
+            })
 
-        cart.subscribe((newCart) => {
-            // @todo: we need anther tracking api to cover async changes
-        })
+            customer.subscribe((newCustomer) => {
+                // @todo: we need anther tracking api to cover async changes
+            })
 
-        customer.subscribe((newCustomer) => {
-            // @todo: we need anther tracking api to cover async changes
-        })
+            const sctp = cart()?.subtotalAmount || ""
+            const cid = customer()?.admetrics?.id || ""
 
-        const sctp = cart()?.subtotalAmount || ""
-        const cid = customer()?.admetrics?.id || ""
+            config.scripts.forEach(script => {
+                const element = document.createElement("script")
+                for (const [key, value] of Object.entries(script.attributes)) {
+                    element.setAttribute(key, value)
+                }
 
-        config.scripts.forEach(script => {
-            const element = document.createElement("script")
-            for (const [key, value] of Object.entries(script.attributes)) {
-                element.setAttribute(key, value)
-            }
+                if (script.inline) {
+                    element.innerText = script.inline.replace("{SCTP}", sctp).replace("{CID}", cid)
+                }
 
-            if (script.inline) {
-                element.innerText = script.inline.replace("{SCTP}", sctp).replace("{CID}", cid)
-            }
-
-            document.body.appendChild(element)
+                document.body.appendChild(element)
+            })
         })
     };
 });
